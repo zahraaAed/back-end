fb5450aa092fe4e9536ed83f819149415b87e9b1
Kamal <kamal_dia@hotmail.com>
2023-10-25 21:05:28 +0300

    done reviews again(kamal)

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -10,12 +10,12 @@ const getCategory=async (req, res) =>{
    
 //get a single one
 const getsCategory=async (req, res) =>{
-    const{id}=req.params
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error :'error in the workout'})
-    }
-    const categories=await Category.findById(id)
-if(!categories){
+  const{id}=req.params
+  if(!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(404).json({error :'error in the workout'})
+  }
+  const categories=await Category.findById(id)
+  if(!categories){
     return res.status(404).json({error :'error in the workout'})
 }
 res.status(200).json(categories)
@@ -24,38 +24,38 @@ res.status(200).json(categories)
 
 //create
 const createCategory=async (req, res) =>{
-    const{categoryName}=req.body
-    try {
-      const categories=await Category.create({categoryName})
-      res.status(200).json(categories)
-    }
-    catch (error){
-      res.status(404).json({error:error.message} )
-
-    }
+  const{categoryName}=req.body
+  try {
+    const categories=await Category.create({categoryName})
+    res.status(200).json(categories)
   }
+  catch (error){
+    res.status(404).json({error:error.message} )
+
+  }
+}
   //delete
   const deleteCategory=async(req,res)=>{
   const{id}=req.params
   if(!mongoose.Types.ObjectId.isValid(id)){
-      return res.status(404).json({error :'error in the workout'})
+    return res.status(404).json({error :'error in the workout'})
   }
   const categories=await Category.findOneAndDelete({_id:id})
-if(!categories){
-  return res.status(404).json({error :'error in the workout'})
+  if(!categories){
+    return res.status(404).json({error :'error in the workout'})
 }
 res.status(200).json(categories)
 }
 
 //update
 const updateCategory=async(req,res)=>{
-    const{id}=req.params
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error :'error in the workout'})
-    }
-    const categories=await Category.findByIdAndUpdate({_id:id},{
-        ...req.body
-    })
+  const{id}=req.params
+  if(!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(404).json({error :'error in the workout'})
+  }
+  const categories=await Category.findByIdAndUpdate({_id:id},{
+    ...req.body
+  })
   if(!categories){
     return res.status(404).json({error :'error in the workout'})  
   }
---
+++ models/reviews(kamal).js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const review_schema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your name"]
+  },
+  body: {
+    type: String,
+    required: [true, "Please enter a review"]
+  },
+  product_id: {
+    type: Number,
+    required: [true, "Please enter product ID"]
+  }
+});
+
+const Review =  mongoose.model("review", review_schema);
+
+// module.exports = Review;
